ee7f1c3ca9935ff372dba6dfc6550943ce430cb0
Tobias Brunsch <tobias.brunsch@aol.de>
2019-01-26 21:39:47 +0100

    MOD ParserConsoleLogger: Support printing log entry counter

--- src/main/java/com/AMS/jBEAM/javaParser/debug/ParserConsoleLogger.java
+++ src/main/java/com/AMS/jBEAM/javaParser/debug/ParserConsoleLogger.java
@@ -1,12 +1,15 @@
 package com.AMS.jBEAM.javaParser.debug;
 
+import com.google.common.base.Strings;
+
 public class ParserConsoleLogger implements ParserLoggerIF
 {
 	private static final int 	SCOPE_INDENT_SIZE	= 2;
 
-	private int	indentSize					= 0;
-	private int numLoggedEntries			= 0;
-	private int	getNumLoggedEntriesToStopAt	= -1;
+	private int		indentSize					= 0;
+	private int		numLoggedEntries			= 0;
+	private int		getNumLoggedEntriesToStopAt	= -1;
+	private boolean	printNumLoggedEntries		= false;
 
 	@Override
 	public void beginChildScope() {
@@ -23,10 +26,13 @@ public class ParserConsoleLogger implements ParserLoggerIF
 		numLoggedEntries++;
 
 		StringBuilder builder = new StringBuilder();
+		if (printNumLoggedEntries) {
+			builder.append(Strings.padStart(String.valueOf(numLoggedEntries), 5, ' ')).append(" ");
+		}
 		for (int i = 0; i < indentSize; i++) {
 			builder.append(" ");
 		}
-		builder.append(formatLogEntry(entry));
+		builder.append(formatLogEntry(entry, builder));
 		System.out.println(builder.toString());
 
 		if (numLoggedEntries == getNumLoggedEntriesToStopAt) {
@@ -44,12 +50,18 @@ public class ParserConsoleLogger implements ParserLoggerIF
 		return this;
 	}
 
+	public ParserConsoleLogger printNumberOfLoggedEntries(boolean print) {
+		this.printNumLoggedEntries = print;
+		return this;
+	}
+
 	private void stop() {
 		// set a break point here to stop at the desired point in time
 		System.out.println("Stopping after " + getNumLoggedEntriesToStopAt + " entries");
 	}
 
-	private static String formatLogEntry(ParserLogEntry entry) {
-		return entry.getContext() + ": " + entry.getLogLevel().getPrefix() + entry.getMessage();
+	private String formatLogEntry(ParserLogEntry entry, StringBuilder builder) {
+		builder.append(entry.getContext()).append(": ").append(entry.getLogLevel().getPrefix()).append(entry.getMessage());
+		return builder.toString();
 	}
 }
