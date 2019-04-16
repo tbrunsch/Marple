de5b01e654c582e28afcad11dd268f663f036f18
Tobias Brunsch <tobias.brunsch@aol.de>
2019-04-17 01:08:32 +0200

    MOD InspectionFrame: Moved navigation buttons to header bar

--- marple-swing/src/dd/kms/marple/swing/gui/InspectionFrame.java
+++ marple-swing/src/dd/kms/marple/swing/gui/InspectionFrame.java
@@ -11,16 +11,17 @@ public class InspectionFrame extends JFrame
 {
 	private static final Dimension INITIAL_SIZE = new Dimension(400, 300);
 
+	private final JPanel		mainPanel			= new JPanel(new BorderLayout());
+
+	private final JPanel		navigationPanel		= new JPanel(new GridBagLayout());
 	private final JButton		prevButton			= new JButton();
 	private final JButton		nextButton			= new JButton();
-
-	private final JPanel		contentPanel		= new JPanel(new BorderLayout());
-
-	private final JPanel		objectOverviewPanel	= new JPanel(new GridBagLayout());
+	private final JPanel		currentObjectPanel	= new JPanel(new GridBagLayout());
 	private final JLabel		classInfoLabel		= new JLabel();
 	private final JLabel		toStringLabel		= new JLabel();
 
-	private final JTabbedPane  	tabbedPane			= new JTabbedPane();
+	private final JTabbedPane 	viewPane			= new JTabbedPane();
+	private final JScrollPane	viewScrollPane		= new JScrollPane(viewPane);
 
 	private final InspectionContext<Component, Component>	inspectionContext;
 
@@ -35,40 +36,38 @@ public class InspectionFrame extends JFrame
 	private void configure() {
 		setTitle("Object Inspection Manager");
 
-		JPanel mainPanel = new JPanel(new GridBagLayout());
 		getContentPane().add(mainPanel);
 
-		int xPos = 0;
-		mainPanel.add(prevButton,   new GridBagConstraints(xPos++, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-		mainPanel.add(contentPanel, new GridBagConstraints(xPos++, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
-		mainPanel.add(nextButton,   new GridBagConstraints(xPos++, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		mainPanel.add(navigationPanel,	BorderLayout.NORTH);
+		mainPanel.add(viewScrollPane, 	BorderLayout.CENTER);
 
-		contentPanel.add(objectOverviewPanel, BorderLayout.NORTH);
-		contentPanel.add(new JScrollPane(tabbedPane), BorderLayout.CENTER);
+		navigationPanel.add(prevButton,   		new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		navigationPanel.add(currentObjectPanel,	new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
+		navigationPanel.add(nextButton,   		new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 
-		objectOverviewPanel.setBorder(BorderFactory.createEtchedBorder());
-		objectOverviewPanel.add(toStringLabel,  new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-		objectOverviewPanel.add(classInfoLabel, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+		currentObjectPanel.setBorder(BorderFactory.createEtchedBorder());
+		currentObjectPanel.add(toStringLabel,  new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+		currentObjectPanel.add(classInfoLabel, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
 
 		setSize(INITIAL_SIZE);
 
-		tabbedPane.addChangeListener(e -> onTabChanged());
+		viewPane.addChangeListener(e -> onTabChanged());
 	}
 
 	public void setViews(Object object, List<? extends Component> views) {
 		initializing = true;
 		toStringLabel.setText('"' + inspectionContext.getDisplayText(object) + '"');
 		classInfoLabel.setText(object.getClass().toString());
-		tabbedPane.removeAll();
+		viewPane.removeAll();
 
 		for (Component view : views) {
-			tabbedPane.add(view, Preconditions.checkNotNull(view.getName(), "Missing name of view '" + view + "'"));
+			viewPane.add(view, Preconditions.checkNotNull(view.getName(), "Missing name of view '" + view + "'"));
 		}
 
 		if (lastSelectedTabTitle != null) {
-			int indexOfLastSelectedTab = tabbedPane.indexOfTab(lastSelectedTabTitle);
+			int indexOfLastSelectedTab = viewPane.indexOfTab(lastSelectedTabTitle);
 			if (indexOfLastSelectedTab >= 0) {
-				tabbedPane.setSelectedIndex(indexOfLastSelectedTab);
+				viewPane.setSelectedIndex(indexOfLastSelectedTab);
 			}
 		}
 		setVisible(true);
@@ -85,9 +84,9 @@ public class InspectionFrame extends JFrame
 		if (initializing) {
 			return;
 		}
-		int selectedIndex = tabbedPane.getSelectedIndex();
+		int selectedIndex = viewPane.getSelectedIndex();
 		if (selectedIndex >= 0) {
-			lastSelectedTabTitle = tabbedPane.getTitleAt(selectedIndex);
+			lastSelectedTabTitle = viewPane.getTitleAt(selectedIndex);
 		}
 	}
 }
