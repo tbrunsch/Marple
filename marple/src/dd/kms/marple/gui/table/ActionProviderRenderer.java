bdb1bfaeffadd51311584db737db4b7957fcef68
Tobias Brunsch <tobias.brunsch@aol.de>
2019-08-05 23:57:21 +0200

    MOD ActionProviderRenderer: Use DefaultTableCellRenderer for coloring logic

--- marple/src/dd/kms/marple/gui/table/ActionProviderRenderer.java
+++ marple/src/dd/kms/marple/gui/table/ActionProviderRenderer.java
@@ -3,18 +3,22 @@ package dd.kms.marple.gui.table;
 import dd.kms.marple.actions.ActionProvider;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
-public class ActionProviderRenderer extends JLabel implements TableCellRenderer
+public class ActionProviderRenderer implements TableCellRenderer
 {
 	private static final Color	DARK_BLUE	= Color.BLUE.darker();
 
+	private final TableCellRenderer	wrappedRenderer	= new DefaultTableCellRenderer();
+
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-		setText(value == null ? null : value.toString());
-		setFont(getFont().deriveFont(0));
-		setForeground(value instanceof ActionProvider ? DARK_BLUE : Color.BLACK);
-		return this;
+		JLabel rendererComponent = (JLabel) wrappedRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		rendererComponent.setText(value == null ? null : value.toString());
+		rendererComponent.setFont(rendererComponent.getFont().deriveFont(0));
+		rendererComponent.setForeground(value instanceof ActionProvider ? DARK_BLUE : Color.BLACK);
+		return rendererComponent;
 	}
 }
