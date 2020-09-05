b5ec2eb73a7c0ce5f470151420b9de5d9e110319
Tobias Brunsch <tobias.brunsch@aol.de>
2020-09-05 14:10:26 +0200

    MOD TestData: Made demo data reproducible (for screenshots)

--- marple-tests/test/dd/kms/marple/TestData.java
+++ marple-tests/test/dd/kms/marple/TestData.java
@@ -5,18 +5,25 @@ import com.google.common.collect.Multimap;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class TestData
 {
-	private final List<Double>				demoList		= IntStream.range(0, 12345).mapToObj(i -> Math.random()).collect(Collectors.toList());
-	private final double[]					demoArray 		= IntStream.range(0, 12345).mapToDouble(i -> Math.random()).toArray();
-	private final Map<Double, String>		demoMap			= IntStream.range(0, 32).mapToObj(i -> Math.random()).collect(Collectors.toMap(d -> d, d -> d.toString()));
-	private final Map<Double, String>		largeDemoMap	= IntStream.range(0, 12345).mapToObj(i -> Math.random()).collect(Collectors.toMap(d -> d, d -> d.toString()));
-	private final Set<String>				demoSet			= IntStream.range(0, 32).mapToObj(i -> String.valueOf(Math.random())).collect(Collectors.toSet());
-	private final Set<String>				largeDemoSet	= IntStream.range(0, 12345).mapToObj(i -> String.valueOf(Math.random())).collect(Collectors.toSet());
+	private static Stream<Double> randomStream(int size) {
+		Random generator = new Random(1234567890L);
+		return IntStream.range(0, size).mapToObj(i -> generator.nextDouble());
+	}
+
+	private final List<Double>				demoList		= randomStream(12345).collect(Collectors.toList());
+	private final double[]					demoArray 		= randomStream(12345).mapToDouble(d -> d).toArray();
+	private final Map<Double, String>		demoMap			= randomStream(32).collect(Collectors.toMap(d -> d, d -> d.toString()));
+	private final Map<Double, String>		largeDemoMap	= randomStream(12345).collect(Collectors.toMap(d -> d, d -> d.toString()));
+	private final Set<String>				demoSet			= randomStream(32).map(String::valueOf).collect(Collectors.toSet());
+	private final Set<String>				largeDemoSet	= randomStream(12345).map(String::valueOf).collect(Collectors.toSet());
 	private final Object					testObject		= demoList;
 	private final Multimap<String, Integer> demoMultimap;
 
