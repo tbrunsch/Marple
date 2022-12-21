38c569a4cd8b99074eebcfc152f31dd3e2f2782c
Tobias Brunsch <tobias.brunsch@aol.de>
2022-12-22 00:54:21 +0100

    MOD VariablePanel: Added column for variable type

--- marple/src/dd/kms/marple/impl/gui/evaluator/VariablePanel.java
+++ marple/src/dd/kms/marple/impl/gui/evaluator/VariablePanel.java
@@ -9,11 +9,12 @@ import dd.kms.marple.api.gui.Disposable;
 import dd.kms.marple.impl.actions.ActionProvider;
 import dd.kms.marple.impl.actions.ActionProviderBuilder;
 import dd.kms.marple.impl.gui.actionproviders.ActionProviderListeners;
-import dd.kms.marple.impl.gui.table.ActionProviderRenderer;
-import dd.kms.marple.impl.gui.table.ColumnDescription;
-import dd.kms.marple.impl.gui.table.ColumnDescriptionBuilder;
-import dd.kms.marple.impl.gui.table.ListBasedTableModel;
+import dd.kms.marple.impl.gui.common.ExceptionFormatter;
+import dd.kms.marple.impl.gui.evaluator.textfields.ClassInputTextField;
+import dd.kms.marple.impl.gui.table.*;
+import dd.kms.zenodot.api.ParseException;
 
+import javax.annotation.Nullable;
 import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.table.TableCellEditor;
@@ -22,9 +23,10 @@ import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.util.List;
 import java.util.*;
+import java.util.function.Consumer;
 
 import static dd.kms.marple.impl.gui.common.GuiCommons.DEFAULT_INSETS;
-import static java.awt.GridBagConstraints.REMAINDER;
+import static java.awt.GridBagConstraints.*;
 
 public class VariablePanel extends JPanel implements Disposable
 {
@@ -33,6 +35,10 @@ public class VariablePanel extends JPanel implements Disposable
 	private final JScrollPane					scrollPane;
 	private final JTable						table;
 	private final ListBasedTableModel<Variable>	tableModel;
+	private final CellEditor					cellEditor;
+
+	private final JPanel						exceptionPanel		= new JPanel(new GridBagLayout());
+	private final JLabel						exceptionLabel		= new JLabel();
 
 	private final JButton						importButton		= new JButton("Import from 'DebugSupport'");
 	private final JButton						exportButton		= new JButton("Export to 'DebugSupport'");
@@ -54,12 +60,22 @@ public class VariablePanel extends JPanel implements Disposable
 
 		TableColumnModel columnModel = table.getColumnModel();
 		columnModel.getColumn(1).setCellRenderer(new ActionProviderRenderer());
+		cellEditor = new CellEditor(this::onException, context);
+		columnModel.getColumn(2).setCellEditor(cellEditor);
+
+		table.setDefaultRenderer(Class.class, new ClassRenderer(context));
 
 		add(scrollPane,		new GridBagConstraints(0, 0, REMAINDER, 1, 1.0, 1.0, GridBagConstraints.NORTHWEST,	GridBagConstraints.BOTH, DEFAULT_INSETS, 0, 0));
 
-		add(importButton,	new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
-		add(exportButton,	new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
-		add(deleteButton,	new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
+		add(exceptionPanel,	new GridBagConstraints(0, 1, REMAINDER, 1, 1.0, 0.0, GridBagConstraints.NORTHWEST,	GridBagConstraints.HORIZONTAL, DEFAULT_INSETS, 0, 0));
+
+		add(importButton,	new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
+		add(exportButton,	new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
+		add(deleteButton,	new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, DEFAULT_INSETS, 0, 0));
+
+		exceptionPanel.setVisible(false);
+		exceptionPanel.add(exceptionLabel,	new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, CENTER, BOTH, DEFAULT_INSETS, 0, 0));
+		exceptionLabel.setForeground(Color.RED);
 
 		updateContent();
 
@@ -97,10 +113,17 @@ public class VariablePanel extends JPanel implements Disposable
 		updateButtons();
 	}
 
+	private void onException(@Nullable Throwable exception) {
+		exceptionPanel.setVisible(exception != null);
+		String exceptionMessage = exception == null ? null : ExceptionFormatter.formatException(exception, true);
+		exceptionLabel.setText(exceptionMessage);
+	}
+
 	private List<ColumnDescription<Variable>> createColumnDescriptions() {
 		return Arrays.asList(
 			new ColumnDescriptionBuilder<>("Name",					String.class, 			Variable::getName)					.editorSettings(this::changeVariableName).build(),
 			new ColumnDescriptionBuilder<>("Value",					ActionProvider.class, 	this::getValueAsActionProvider).build(),
+			new ColumnDescriptionBuilder<>("Type",					Class.class, 			Variable::getType)					.editorSettings(this::changeType).build(),
 			new ColumnDescriptionBuilder<>("final",					Boolean.class,			Variable::isFinal)					.editorSettings(this::changeIsFinal).build(),
 			new ColumnDescriptionBuilder<>("Use hard reference",	Boolean.class,			Variable::isUseHardReference)		.editorSettings(this::changeUseHardReference).build()
 		);
@@ -174,6 +197,23 @@ public class VariablePanel extends JPanel implements Disposable
 		tableModel.fireTableChanged(new TableModelEvent(tableModel));
 	}
 
+	private void changeType(List<Variable> variables, int elementIndex, Object typeAsObject) {
+		if (!(typeAsObject instanceof Class<?>)) {
+			return;
+		}
+		Variable oldVariable = variables.get(elementIndex);
+		Object value = oldVariable.getValue();
+		Class<?> type = (Class<?>) typeAsObject;
+		if (!type.isInstance(value)) {
+			Exception exception = new IllegalArgumentException("The current variable value is no instance of " + type.getName());
+			onException(exception);
+			return;
+		}
+		Variable newVariable = Variable.create(oldVariable.getName(), type, oldVariable.getValue(), oldVariable.isFinal(), oldVariable.isUseHardReference());
+		variables.set(elementIndex, newVariable);
+		onException(null);
+	}
+
 	private void changeVariableName(List<Variable> variables, int elementIndex, Object nameAsObject) {
 		if (!(nameAsObject instanceof String)) {
 			return;
@@ -227,5 +267,49 @@ public class VariablePanel extends JPanel implements Disposable
 	@Override
 	public void dispose() {
 		variables.clear();
+	}
+
+
+	private static class CellEditor extends AbstractCellEditor implements TableCellEditor
+	{
+		private final ClassInputTextField		editorTextField;
+
+		private final Map<Integer, String>		rowToTypeNames = new HashMap<>();
+
+		private boolean							editingFinished	= false;
+		private int								currentRow		= -1;
+
+		CellEditor(Consumer<Throwable> exceptionConsumer, InspectionContext context) {
+			editorTextField = new ClassInputTextField(context);
+			editorTextField.requestFocus();
+			editorTextField.setEvaluationResultConsumer(o -> {
+				editingFinished = true;
+				rowToTypeNames.put(currentRow, editorTextField.getText());
+				stopCellEditing();
+			});
+			editorTextField.setExceptionConsumer(exceptionConsumer);
+		}
+
+		@Override
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+			currentRow = row;
+			editingFinished = false;
+			editorTextField.setText(value instanceof Class<?> ? ((Class<?>) value).getName() : String.valueOf(value));
+			return editorTextField;
+		}
+
+		@Override
+		public Object getCellEditorValue() {
+			try {
+				return editorTextField.evaluateText();
+			} catch (ParseException e) {
+				return null;
+			}
+		}
+
+		@Override
+		public boolean stopCellEditing() {
+			return editingFinished && super.stopCellEditing();
+		}
 	}
 }
