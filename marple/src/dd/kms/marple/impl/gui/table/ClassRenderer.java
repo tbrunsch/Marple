47bf9b67f7487a4a3e4996401ce84f68dc833338
Tobias Brunsch <tobias.brunsch@aol.de>
2022-12-25 22:13:51 +0100

    MOD ClassRenderer, MemberRenderer: Corrected foreground and background colors of selected cells

--- marple/src/dd/kms/marple/impl/gui/table/ClassRenderer.java
+++ marple/src/dd/kms/marple/impl/gui/table/ClassRenderer.java
@@ -1,6 +1,7 @@
 package dd.kms.marple.impl.gui.table;
 
 import dd.kms.marple.api.InspectionContext;
+import dd.kms.marple.impl.gui.common.GuiCommons;
 
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
@@ -12,12 +13,20 @@ public class ClassRenderer extends JLabel implements TableCellRenderer
 
 	public ClassRenderer(InspectionContext context) {
 		this.context = context;
+		setOpaque(true);
 	}
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 		setText(context.getDisplayText(value));
-		setFont(getFont().deriveFont(0));
+		setFont(getFont().deriveFont(Font.PLAIN));
+		if (isSelected) {
+			setForeground(GuiCommons.getSelectedTableForegroundColor());
+			setBackground(GuiCommons.getSelectedTableBackgroundColor());
+		} else {
+			setForeground(table.getForeground());
+			setBackground(table.getBackground());
+		}
 		return this;
 	}
 }
--- marple/src/dd/kms/marple/impl/gui/table/MemberRenderer.java
+++ marple/src/dd/kms/marple/impl/gui/table/MemberRenderer.java
@@ -7,18 +7,29 @@ import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.lang.reflect.Member;
+import java.util.Objects;
 
 public class MemberRenderer extends JLabel implements TableCellRenderer
 {
+	public MemberRenderer() {
+		setOpaque(true);
+	}
+
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 		Member member = (Member) value;
 		AccessModifier accessModifier = AccessModifier.getValue(member.getModifiers());
 		setText(accessModifier.toString());
-		Color color = GuiCommons.getAccessModifierColor(accessModifier);
-		setFont(getFont().deriveFont(0));
-		setBackground(color == null ? Color.BLACK : color);
-		setOpaque(true);
+		Color bg = GuiCommons.getAccessModifierColor(accessModifier);
+		setFont(getFont().deriveFont(Font.PLAIN));
+		Color fg = table.getForeground();
+		if (isSelected) {
+			setForeground(GuiCommons.getSelectedTableForegroundColor(fg));
+			setBackground(GuiCommons.getSelectedTableBackgroundColor(bg));
+		} else {
+			setForeground(fg);
+			setBackground(bg);
+		}
 		return this;
 	}
 }
